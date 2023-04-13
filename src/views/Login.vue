3e81e8c383041a8d2a235d4ee896b5524324eb9a
pjp <2736045645@qq.com>
2023-04-13 10:18:50 +0800

    登录

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="login">
-        <el-form :model="form" :rules="rules" label-width="120px">
+        <el-form :model="form" :rules="rules" ref="FormRef" label-width="120px">
           <el-form-item label="用户名" prop="username">
             <el-input v-model="form.username" />
           </el-form-item>
           <el-form-item label="密码" prop="password">
-            <el-input v-model="form.password" />
-          </el-form-item>
+            <el-input v-model="form.password"/>
+          </el-form-item>        
           <el-form-item>
-      <el-button type="primary" @click="onSubmit">登录</el-button>
+      <el-button type="primary" @click="onSubmit(FormRef)">登录</el-button>
     </el-form-item>
         </el-form>
     </div>
@@ -16,7 +16,10 @@
 
 <script setup>
 
-import { reactive } from 'vue'
+import { trigger } from '@vue/reactivity';
+import { reactive, ref} from 'vue';
+
+const FormRef = ref()
 
 const form = reactive({
   username:'',
@@ -26,13 +29,25 @@ const form = reactive({
 const rules = reactive({
   username:[
   { required: true, message: '用户名不能为空', trigger: 'blur' },
+  { pattern: /^[a-zA-Z0-9_-]{4,16}$/,message: '您的用户名格式有误',trigger: 'blur'}
   ],
   password:[
-  { required: true, message: '密码不能为空', trigger: 'blur' }
+  { required: true, message: '密码不能为空', trigger: 'blur' },
+  { pattern: /^.*(?=.{6,})(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*? ]).*$/,message: '您的密码格式有误',trigger: 'blur'}
   ]
 })
 
-const onSubmit = () => {}
+const onSubmit = (formEl) => {
+
+  if (!formEl) return
+  formEl.validate((valid, fields) => {
+    if (valid) {
+      console.log('submit!',form)
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
+}
 </script>
 
 <style lang="scss" scoped>
